f3a8abdef583514dcb587e2dea9144c3358c19fd
Vadim Solovyev <71266347+ssssddds22@users.noreply.github.com>
2024-12-09 20:33:27 +0300

    Update views.py

--- project1/my_site/main/views.py
+++ project1/my_site/main/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 def index(request):
     return render(request, "main/index.html")
 
+def page(request):
+    return render(request, "main/page.html")
+
+def page1(request):
+    return render(request, "main./page1.html")
+
 # Create your views here.
