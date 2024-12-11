806debd57789b73e1250519095108d61d5a65e63
oozzyyy <ivanzerebcov20008@gmail.com>
2024-12-11 22:24:55 +0300

    3

--- project1/my_site/main/urls.py
+++ project1/my_site/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('index', views.index ),
+    path('index', views.index),
     path('page', views.page),
     path('page1', views.page1),
     path('page2', views.page2),
